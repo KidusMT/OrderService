d5cc3ddf5e6916d4019fd8e5997bea11c75801da
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-08-10 00:48:54 -0500

    first commit

---
+++ src/test/java/project/swa/OrderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.swa.OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
